a3f22147dd6b2ec95fe8b1f306eab2ed3d4e1426
CaptnSwift <33681728+CaptnSwift@users.noreply.github.com>
2021-03-30 12:37:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,18 @@ from dash.dependencies import Input, Output
 def serve_layout():
     values = support.pull_data()
 
-    data = support.clean_data(values)
+    data, dates = support.clean_data(values)
 
     islands, island_data = support.seperate_islands(data)
 
     rect_dict = support.generate_rects(data)
 
-    fig = support.all_chart(islands, island_data, rect_dict)
+    all_chart = support.all_chart(islands, island_data, rect_dict)
+
+    minis = support.chart_island_minis(islands, island_data)
+
+    """ TODO add a filter to so you can look at only one island at a time """
+    table = support.daily_table(islands, island_data, dates)
 
     return html.Div(children=[
                 dcc.Markdown(children=
@@ -35,8 +40,20 @@ def serve_layout():
 
                 dcc.Graph(
                     id='all-chart',
-                    figure=fig
+                    figure=all_chart
                 ),
+
+                dcc.Graph(
+                    id='minis',
+                    figure=minis
+                ),
+
+                dcc.Graph(
+                    id='table',
+                    figure=table
+                ),
+
+
     ])
 
 # %%
